2aafb384317ef2c66174cbdfafe760ddaad282f8
MaheenHannan-acvt712 <74509192+MaheenHannan-acvt712@users.noreply.github.com>
2023-07-30 00:10:29 +0100

    Create network_trainer.py

---
+++ Task2/network_trainer.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class NetworkTrainer:
+    def __init__(self, neural_network):
+        self.neural_network = neural_network
+
+    def train_network(self,
+                      
+                      data_train, labels_train,
+                      data_test, label_test,
+                      
+                      learning_rate=0.1,
+                      epochs=1,
+                      batch_size=32):
+        
+        for epoch in range(epochs):
+            
+            array = data_train.shape[0]
+            batches = np.arange(array)
+            
+            np.random.shuffle(batches)
+            
+            for batch in range(0, array, batch_size):
+                eob = batch + batch_size
+                mini_batch = batches[batch:eob]
+                data_batch = data_train[mini_batch]
+                labels_batch = labels_train[mini_batch]
+
+                model_labels = self.neural_network.forward(data_batch)
+                
+                self.neural_network.backward(data_batch, labels_batch,
+                                             model_labels, learning_rate)
+
+                loss = self.neural_network.calculate_loss(data_train, labels_train)
+                
+                model_label = self.neural_network.predict(data_test)
+                accuracy = np.mean(model_label == np.argmax(label_test, axis=1))
+                
+                epoch_print = f"Epoch {epoch+1}/{epochs}"
+                step_print = f"Step: {int(batch/batch_size)}"
+                loss_print = f"Loss: {loss:.4f}"
+                test_print = f"Accuracy: {accuracy:.4f}"
+                
+                print(epoch_print, step_print, loss_print, test_print)
